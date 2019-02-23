6c6c242ccd931a856217d8de242096b93d96e380
Camilo Silva <camilodsmp@gmail.com>
2019-02-22 22:35:00 -0300

    Implemented test cases.
    Still trying to understand how them work.

---
+++ src/test/java/com/carrental/application/customer/CustomerControllerIntegrationTest.java
@@ -0,0 +1,41 @@
+package com.carrental.application.customer;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CustomerControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/apis/customers/");
+    }
+
+    @Test
+    public void createAndRetriveCustomer() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString(),
+                String.class);
+        assertThat(response.getBody(), is("[]"));
+    }
+}
--- src/test/java/com/carrental/application/customer/CustomerRepositoryIntegrationTest.java
+++ src/test/java/com/carrental/application/customer/CustomerRepositoryIntegrationTest.java
@@ -1,36 +1,41 @@
 package com.carrental.application.customer;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.List;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource
 public class CustomerRepositoryIntegrationTest {
 
     @Autowired
     private TestEntityManager entityManager;
 
     @Autowired
-    private CustomerRepository customerRepository;
+    private MockMvc mvc;
+
+    @Autowired
+    private CustomerRepository repository;
 
     @Test
-    public void whenFindByName_thenReturnCustomer() {
-        Customer customer = new Customer("John", "Doe");
-        entityManager.persist(customer);
-        entityManager.flush();
-
-        List<Customer> customers = customerRepository.findByFirstName("John");
-
-        assertThat(customers)
-            .isNotEmpty()
-            .contains(customer);
+    public void createAndRetriveCustomer() throws Exception {
+        mvc.perform(
+            MockMvcRequestBuilders
+                .get("http://localhost:8080/apis/customers/")
+                .accept(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isOk());
     }
 }
